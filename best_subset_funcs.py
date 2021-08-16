fixed normalisation of weights

best_subset_funcs.py
@@ -301,16 +301,17 @@ def neighs_subset2D(arr, bins, factor, t1, thresh):
     
     cnt = 0
     split_points = []
-    for b in sbins:
+    for b in sbins[:-1]:
         cnt += len(b)
         split_points.append(cnt)
     sbins_concat = np.concatenate(sbins, axis=None)
     sbins_idx = np.split(np.arange(len(sbins_concat)), split_points)
     weights_concat = np.concatenate(weights, axis=None)
+    assert weights_concat.sum() == float(len(arr))  # comment out later
     
     cnt = 0
     split_points = []
-    for b in abins:
+    for b in abins[:-1]:
         cnt += len(b)
         split_points.append(cnt)
     abins_idx = np.split(np.arange(len(arr)), split_points)
@@ -352,8 +353,8 @@ def neighs_subset2D(arr, bins, factor, t1, thresh):
             nabc = abc[neighs].sum() 
             d = nabc - nsbc*factor
             if round(d/factor) > 0:
-                add_to = np.concatenate([sbins_idx[n] for n in neighs])
-                pool = [i for i in np.concatenate([abins_idx[n] for n in neighs]) if i not in add_to]
+                add_to = [i for i in np.concatenate([sbins_idx[n] for n in neighs]) if i not in todel]
+                pool = [i for i in np.concatenate([abins[n] for n in neighs]) if i not in add_to]
                 sample = random.sample(pool, round(d/factor))
                 toadd.extend(sample)  
                 additions.append((add_to, sample))

test.py
@@ -20,15 +20,17 @@ from scipy.sparse import csr_matrix
 import best_subset_funcs as fncs
 import copy as cp
 
-fname = "/home/nico/WORK/HUJI/cis-trans/trans_iso1.csv"
+fname = "/home/nico/WORK/HUJI/cis-trans/9cis_iso1.csv"
 arr = pd.read_csv(fname,index_col=0)[["ex_en", "osc"]].values
 
+#t1 = 0.0001
+#thresh = 0.001
 t1 = 0.0001
-thresh = 0.001
-bins = 5
-factor = 4
-concat = False
 
+thresh = 0.001
+bins = 10
+factor = 5
+concat = False
 
 sbins, weights = fncs.subset2D(arr, bins, factor, t1, thresh, concat=False)
 sbc = np.array([len(i) for i in sbins])
@@ -38,16 +40,18 @@ diff = abc - sbc*factor
 
 cnt = 0
 split_points = []
-for b in sbins:
+for b in sbins[:-1]:
     cnt += len(b)
     split_points.append(cnt)
 sbins_concat = np.concatenate(sbins, axis=None)
 sbins_idx = np.split(np.arange(len(sbins_concat)), split_points)
 weights_concat = np.concatenate(weights, axis=None)
+assert weights_concat.sum() - float(len(arr)) < 1e-6 # comment out later
+print("1", weights_concat.sum())
 
 cnt = 0
 split_points = []
-for b in abins:
+for b in abins[:-1]:
     cnt += len(b)
     split_points.append(cnt)
 abins_idx = np.split(np.arange(len(arr)), split_points)
@@ -65,6 +69,8 @@ for n in range(-factor + 1, 0):
         done.extend(neighs)
         nsbc = sbc[neighs].sum()
         nabc = abc[neighs].sum() 
+        if nabc == 1:
+            continue
         d = nsbc*factor - nabc
         if round(d/factor) > 0:
             pool = np.concatenate([sbins_idx[n] for n in neighs]).tolist()
@@ -72,10 +78,9 @@ for n in range(-factor + 1, 0):
             if len(pool) == topick:
                 topick -= 1
             sample = random.sample(pool, topick)
-            if sample:
-                todel.extend(sample)
-                unsampled = [i for i in pool if i not in sample]
-                deletions.append((unsampled, sample))
+            todel.extend(sample)
+            unsampled = [i for i in pool if i not in sample]
+            deletions.append((unsampled, sample))
 toadd = []
 done = []
 additions = []
@@ -90,8 +95,8 @@ for n in range(1, factor):
         nabc = abc[neighs].sum() 
         d = nabc - nsbc*factor
         if round(d/factor) > 0:
-            add_to = np.concatenate([sbins_idx[n] for n in neighs])
-            pool = [i for i in np.concatenate([abins_idx[n] for n in neighs]) if i not in add_to]
+            add_to = [i for i in np.concatenate([sbins_idx[n] for n in neighs]) if i not in todel]
+            pool = [i for i in np.concatenate([abins[n] for n in neighs]) if i not in add_to] #asdf
             sample = random.sample(pool, round(d/factor))
             toadd.extend(sample)  
             additions.append((add_to, sample))
@@ -99,24 +104,21 @@ for n in range(1, factor):
 indexes = [i for i in range(len(sbins_concat)) if i not in todel]
 sbst = sbins_concat.copy()[indexes]
 sbst = np.append(sbst, toadd)
+print("weight that will be removed", weights_concat.sum() - weights_concat[indexes].sum())
+print("weight that will be added", len(toadd))
 for del_ in deletions:
+    print("will remove", weights_concat[del_[1]].sum())
+#    print("adding to each unsampled", weights_concat[del_[1]].sum()/len(weights_concat[del_[0]]))
     weights_concat[del_[0]] += weights_concat[del_[1]].sum()/len(weights_concat[del_[0]])
+print("2", weights_concat.sum())
 for add in additions:
+    print("will add", len(add[1]))
+#    print("removing from each unsampled", len(add[1])/len(weights_concat[add[0]]))
     weights_concat[add[0]] -= len(add[1])/len(weights_concat[add[0]])
-#nweights = weights_concat[indexes]
-#nweights = np.append(nweights, np.ones(len(toadd)))
-#sbst = sbst.astype("int")
-#
-##sbst, weights = fncs.neighs_subset2D(arr, bins, factor, t1, thresh)
-##abins = fncs.get_bins(arr, bins=bins, flatten=True)
-#range_ = [[arr[:,0].min(),arr[:,0].max()], [arr[:,1].min(), arr[:,1].max()]]
-#asbst = arr[sbst]
-#sbins2 = fncs.get_bins2D(asbst, bins=bins, range_=range_, flatten=True)
-#sum_ = 0
-#for n in range(len(sbins2)):
-#    sb, ab = sbins2[n], abins[n]
-#    if sb.size > 0 or ab.size > 0:
-#        sum_ += nweights[sb].sum()
-##        print("array", ab.size,"sum weights", ws[sb].sum())
-#        if abs(ab.size - nweights[sb].sum()) > 0.01:
-#            print(n, "array", ab.size,"sum weights", nweights[sb].sum())
+print("3", weights_concat.sum())
+#nweights = weights_concat
+nweights = weights_concat[indexes]
+print("4", nweights.sum())
+nweights = np.append(nweights, np.ones(len(toadd)))
+sbst = sbst.astype("int")
+print("sum is {:.5f}".format(nweights.sum()))

test2.py
@@ -40,7 +40,7 @@ for iso_f, emb_f in zip(iso_fs, emb_fs):
         for factor in [3,4,5]:
             t1 = 0.0001
             thresh = 0.001
-            itr = 250
+            itr = 3
             for arr, arr_name in zip([isos, embs], ["iso", "emb"]):
                 deltaN = np.zeros([itr,2])
                 deltaS = np.zeros([itr,2])
@@ -57,67 +57,69 @@ for iso_f, emb_f in zip(iso_fs, emb_fs):
                 best_dN = deltaN[idx]
                 sbst = sbst_lst[idx]
                 ws =ws_lst[idx]
-                jsfp = os.path.join(sbstfol, "{}_{}_weighted_{}.json".format(system, arr_name, factor))
+                jsfp = os.path.join(sbstfol, "{}_{}_todel_{}.json".format(system, arr_name, factor))
                 with open(jsfp, "w") as  f:
+                    print(system, arr_name, factor, ws.sum())
                     js.dump([sbst.tolist(), ws.tolist()], f)
     else:
         fol, fname = os.path.split(iso_f)
         for factor in [3,4,5]:
             for arr, arr_name in zip([isos, embs], ["iso", "emb"]):
-                jsfp = os.path.join(sbstfol, "{}_{}_weighted_{}.json".format(system, arr_name, factor))
+                jsfp = os.path.join(sbstfol, "{}_{}_todel_{}.json".format(system, arr_name, factor))
                 with open(jsfp, "r") as  f:
                     sbst, ws = np.array(js.load(f))
                 sbst = sbst.astype("int")
-                plotbins = 10
-                fig = plt.figure(figsize=(20, 10), dpi=150)
-                ax = fig.add_subplot(221)
-                _, bins, _t = ax.hist(arr[:,0], bins=plotbins, alpha=1, density=True,
-                                      color="red", histtype="step", linewidth=5, label="full")
-                ax.hist(arr[:,0][sbst], weights=ws, bins=bins, alpha=0.5, density=True,
-                        color="green", histtype="step", linewidth=5, linestyle="-", label="subset")
-                ax.set_xlabel(r"$\varepsilon$")
-                ax.set_ylabel("occurrence")
-                ax.legend()
-                ax.set_title(r"$\Delta <\varepsilon^{{{lbl}}}>$ = {d1:.5f}   $\Delta <f^{{{lbl}}}>$ = {d2:.5f}".format(**{
-                        "d1": np.average(arr[:,0], weights=arr[:,1]) - np.average(arr[:,0][sbst], weights=arr[:,1][sbst]*ws),
-                        "d2": np.average(arr[:,1]) - np.average(arr[:,1][sbst],  weights=ws),
-                        "lbl": arr_name}))
-                ax2 = fig.add_subplot(222)
-                _, bins, _t = ax2.hist(arr[:,1], bins=plotbins, alpha=1, density=True,
-                                       color="red", histtype="step", linewidth=5, label="full")
-                ax2.hist(arr[:,1][sbst], weights=ws, bins=bins, alpha=0.5, density=True,
-                         color="green", histtype="step", linewidth=5, linestyle="-", label="subset")
-                ax2.set_xlabel("$f$")
-                ax2.set_ylabel("occurrence")
-                ax2.legend()
-                other_arr = embs if arr_name == "iso" else isos
-                other_name = "emb" if arr_name == "iso" else "iso"
-                ax2.set_title(r"$\Delta <\varepsilon^{{{lbl}}}>$ = {d1:.5f}   $\Delta <f^{{{lbl}}}>$ = {d2:.5f}".format(**{
-                        "d1": np.average(other_arr[:,0], weights=other_arr[:,1]) - np.average(other_arr[:,0][sbst], weights=other_arr[:,1][sbst]*ws),
-                        "d2": np.average(other_arr[:,1]) - np.average(other_arr[:,1][sbst], weights=ws),
-                        "lbl": other_name}))
-                ax3 = fig.add_subplot(223)
-                Z3, xedges3, yedges3 = np.histogram2d(*arr.T, bins=10, density=True)
-#                h3 = ax3.hist2d(*arr.T, bins=10, alpha=0.5, density=True)
-                h3 = ax3.pcolormesh(xedges3, yedges3, Z3)
-                ax3.set_xlabel(r"$\varepsilon$")
-                cb = fig.colorbar(h3, ax=ax3)
-#                cb = fig.colorbar(h3[3], ax=ax3)
-                density = cb.get_ticks()
-                ax3.set_ylabel(r"$f$")
-                ax3.set_title("full")
-                ax4 = fig.add_subplot(224)
-#                h4 = ax4.hist2d(*arr[sbst].T, weights=ws, bins=10, alpha=0.5, density=True)
-                Z4, xedges4, yedges4 = np.histogram2d(*arr[sbst].T, weights=ws, bins=10, density=True)
-                h4 = ax4.pcolormesh(xedges4, yedges4, Z4, vmin=density.min(), vmax=density.max())
-                ax4.set_xlabel(r"$\varepsilon$")
-                fig.colorbar(h4, ax=ax4)
-#                fig.colorbar(h4[3], ax=ax4)
-                ax4.set_ylabel(r"$f$")
-                ax4.set_title("subset")
-                fig.suptitle("{}, factor = {}".format(arr_name, factor))
-                fig.subplots_adjust(left=0.05, right=0.99, top=0.9, bottom=0.075, wspace=0.25, hspace=0.3)
-                fig.savefig(jsfp[:-4]+"png")
+                print(system, factor, ws.sum())
+#                plotbins = 10
+#                fig = plt.figure(figsize=(20, 10), dpi=150)
+#                ax = fig.add_subplot(221)
+#                _, bins, _t = ax.hist(arr[:,0], bins=plotbins, alpha=1, density=True,
+#                                      color="red", histtype="step", linewidth=5, label="full")
+#                ax.hist(arr[:,0][sbst], weights=ws, bins=bins, alpha=0.5, density=True,
+#                        color="green", histtype="step", linewidth=5, linestyle="-", label="subset")
+#                ax.set_xlabel(r"$\varepsilon$")
+#                ax.set_ylabel("occurrence")
+#                ax.legend()
+#                ax.set_title(r"$\Delta <\varepsilon^{{{lbl}}}>$ = {d1:.5f}   $\Delta <f^{{{lbl}}}>$ = {d2:.5f}".format(**{
+#                        "d1": np.average(arr[:,0], weights=arr[:,1]) - np.average(arr[:,0][sbst], weights=arr[:,1][sbst]*ws),
+#                        "d2": np.average(arr[:,1]) - np.average(arr[:,1][sbst],  weights=ws),
+#                        "lbl": arr_name}))
+#                ax2 = fig.add_subplot(222)
+#                _, bins, _t = ax2.hist(arr[:,1], bins=plotbins, alpha=1, density=True,
+#                                       color="red", histtype="step", linewidth=5, label="full")
+#                ax2.hist(arr[:,1][sbst], weights=ws, bins=bins, alpha=0.5, density=True,
+#                         color="green", histtype="step", linewidth=5, linestyle="-", label="subset")
+#                ax2.set_xlabel("$f$")
+#                ax2.set_ylabel("occurrence")
+#                ax2.legend()
+#                other_arr = embs if arr_name == "iso" else isos
+#                other_name = "emb" if arr_name == "iso" else "iso"
+#                ax2.set_title(r"$\Delta <\varepsilon^{{{lbl}}}>$ = {d1:.5f}   $\Delta <f^{{{lbl}}}>$ = {d2:.5f}".format(**{
+#                        "d1": np.average(other_arr[:,0], weights=other_arr[:,1]) - np.average(other_arr[:,0][sbst], weights=other_arr[:,1][sbst]*ws),
+#                        "d2": np.average(other_arr[:,1]) - np.average(other_arr[:,1][sbst], weights=ws),
+#                        "lbl": other_name}))
+#                ax3 = fig.add_subplot(223)
+#                Z3, xedges3, yedges3 = np.histogram2d(*arr.T, bins=10, density=True)
+##                h3 = ax3.hist2d(*arr.T, bins=10, alpha=0.5, density=True)
+#                h3 = ax3.pcolormesh(xedges3, yedges3, Z3)
+#                ax3.set_xlabel(r"$\varepsilon$")
+#                cb = fig.colorbar(h3, ax=ax3)
+##                cb = fig.colorbar(h3[3], ax=ax3)
+#                density = cb.get_ticks()
+#                ax3.set_ylabel(r"$f$")
+#                ax3.set_title("full")
+#                ax4 = fig.add_subplot(224)
+##                h4 = ax4.hist2d(*arr[sbst].T, weights=ws, bins=10, alpha=0.5, density=True)
+#                Z4, xedges4, yedges4 = np.histogram2d(*arr[sbst].T, weights=ws, bins=10, density=True)
+#                h4 = ax4.pcolormesh(xedges4, yedges4, Z4, vmin=density.min(), vmax=density.max())
+#                ax4.set_xlabel(r"$\varepsilon$")
+#                fig.colorbar(h4, ax=ax4)
+##                fig.colorbar(h4[3], ax=ax4)
+#                ax4.set_ylabel(r"$f$")
+#                ax4.set_title("subset")
+#                fig.suptitle("{}, factor = {}".format(arr_name, factor))
+#                fig.subplots_adjust(left=0.05, right=0.99, top=0.9, bottom=0.075, wspace=0.25, hspace=0.3)
+#                fig.savefig(jsfp[:-4]+"png")
 
 #fig2 = plt.figure(figsize=(20, 10), dpi=150)
 #ax = fig2.add_subplot(111)